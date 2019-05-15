25512f4c9784f4915ab6003e52a3b4fac5a81f05
lqp <liuqipeng@youganw.com>
2019-05-15 14:31:24 +0800

    上传代码

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    Hello World!
+    <span>sssssssss</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.app {
+  color: skyblue;
+  span {
+    color: red;
+  }
+}
+</style>
